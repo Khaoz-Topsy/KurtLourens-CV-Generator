279c433933a2789ac0aa47cbfd0cbe00d9d06420
Kurt Lourens <kurt.lourens@entelect.co.za>
2020-12-31 15:31:01 +0200

    change how links are rendered

--- KhaozNet.CV.Domain/Entity/CvDetail.cs
+++ KhaozNet.CV.Domain/Entity/CvDetail.cs
@@ -10,5 +10,26 @@ namespace KhaozNet.CV.Domain.Entity
 
         [JsonProperty("descriptions")]
         public List<string> Descriptions { get; set; }
+
+        [JsonProperty("socialLinks")]
+        public List<SocialLink> SocialLinks { get; set; }
+    }
+
+    public class SocialLink
+    {
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("classNames")]
+        public string ClassNames { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("onClick")]
+        public string OnClick { get; set; }
     }
 }
